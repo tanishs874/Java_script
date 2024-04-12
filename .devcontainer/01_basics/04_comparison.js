ac6c9f93ccae0ad2f5499cf74e555e69bf97aa6e
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2024-04-12 10:41:16 +0000

    done

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(1>2) 
+// console.log(1<=2)
+// console.log(1!=0)
+// console.log(1==7)
+
+console.log("2">1) // -> True
+console.log("02">1) // -> True
+
+console.log(null>0) // -> False
+console.log(null==0) // -> False
+console.log(null>=0) // -> True
+
+console.log(undefined>0) //-> False
+console.log(undefined<0) // -> False
+console.log(undefined==0) // ->False
+
+/* === triple equals strict check */
+
+console.log("2"==2)
+console.log("2"===2)
+
