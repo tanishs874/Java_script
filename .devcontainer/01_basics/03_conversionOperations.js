ee6a8a9fe74513f100552f4f64482ae7d6bd1b28
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2024-04-12 06:55:32 +0000

    done

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueofScore = Number(score)
+console.log(typeof valueofScore)
+console.log(valueofScore)
+
+// "33" -> 33
+// "33abc" -> Nan
+// true -> 1 , false -> 0
+
+let isloggedIn = "tanish"
+let booleanisLoggedIn = Boolean(isloggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 -> True
+// 0 -> False
+// "" -> False
+// "Tanish" -> True
+
+let somenumber = 32
+let stringnumber = String(somenumber)
+console.log(typeof stringnumber)
