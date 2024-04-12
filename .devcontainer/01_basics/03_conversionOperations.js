986d4bd81e03d70b108709ab8ccc4b3a63c7b4bf
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2024-04-12 07:11:53 +0000

    done

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -23,3 +23,39 @@ console.log(booleanisLoggedIn)
 let somenumber = 32
 let stringnumber = String(somenumber)
 console.log(typeof stringnumber)
+
+
+/******************************OPERATIONS ************************************* */ 
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+*/
+
+let str1 = "Tanish"
+let str2 = "sharma"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2) // ->12
+console.log(1+"2") // ->12
+console.log("1"+ 2 + 2) // ->122
+console.log(1 + 2 + "2") // ->32
+
+console.log(true) // ->true
+console.log(+true) // -> 1
+console.log(+"") // -> 0
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
