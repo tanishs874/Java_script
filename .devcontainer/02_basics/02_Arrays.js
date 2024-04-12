06d16fe88b88cb80aede7f6e1b85cee4e359fdbe
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2024-04-12 16:29:27 +0000

    done

---
+++ .devcontainer/02_basics/02_Arrays.js
@@ -0,0 +1,37 @@
+const marvel_heroes = ["Thor","Ironman","Spiderman"]
+const dc_heroes = ["Superman","flash","batman"]
+
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes)
+//The Push method will create a array within a array and will show the result like this 
+//[ 'Thor', 'IronMan', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ]
+ 
+// console.log(marvel_heroes[3][1]);//flash
+
+const all_heroes = marvel_heroes.concat(dc_heroes)
+console.log(all_heroes);
+
+//On the other side the concat will show the result like this
+//[ 'Thor', 'IronMan', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
+
+//Spread operator 
+
+const all_new_heroes = [...marvel_heroes,...dc_heroes]
+console.log(all_new_heroes);
+
+//[ 'Thor', 'IronMan', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
+//Spread operator is also same as concat 
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Tanish"))
+console.log(Array.from("Tanish"))
+console.log(Array.from({name:"Tanish"}))//Interesting  case it will return [] empty
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))//[ 100, 200, 300 ] 
