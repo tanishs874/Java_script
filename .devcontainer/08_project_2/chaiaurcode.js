48a693b9a15a5fcf67ad0f011d8fdd49c3807dd9
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2024-04-16 18:24:59 +0000

    done

---
+++ .devcontainer/08_project_2/chaiaurcode.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if(height === '' || height < 0 || isNaN(height)){
+    results.innerHTML = `Please enter a valid height ${height}`
+  }
+  else if(weight === '' || weight < 0 || isNaN(weight)){
+    results.innerHTML = `Please enter a valid weight ${weight}`
+  }
+  else{
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    // Show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+})
