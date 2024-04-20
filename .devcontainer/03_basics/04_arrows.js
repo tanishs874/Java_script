1c9f059b0aeccc314f4a1c34ec36c25f1a7409a8
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2024-04-20 06:00:59 +0000

    done

--- .devcontainer/03_basics/04_arrows.js
+++ .devcontainer/03_basics/04_arrows.js
@@ -2,8 +2,8 @@ const user = {
     username:"Tanish",
     price:20,
     welcomegreeting : function(){
-        // console.log(`${this.username} welcome to website`);
-        // console.log(this);
+        console.log(`${this.username} welcome to website`);
+        console.log(this);
     }
 }
 user.welcomegreeting;// Nothing will print here
@@ -17,46 +17,39 @@ user.welcomegreeting();//ram welcome to website
 
 // function chai(){
 //     let username = "sharma"
-//     console.log(this.username);//username will be undefined here 
+//     console.log(this.username);//username will be undefined here //this can be used in objects but not in functions
 // }
 // chai()
-
-// const chai = function(){
-//     let username = "sharma"
-//     console.log(this.user)//username will be undefined here 
-// }
-// chai()
-
 
 //Arrow function
 
 const chai = () => {
     let username = "sharma"
     console.log(this);//{}
-    console.log(this.username);//undefined
+    console.log(this.username);//undefined//We cannot use this also in arrow function
 }
 chai()
 
-const addtwo = (num1 , num2) => {//This is a simple arrow function
-    return num1+num2
-}
-console.log(addtwo(4,5));//9 
+// const addtwo = (num1 , num2) => {//This is a simple arrow function
+//     return num1+num2
+// }
+// console.log(addtwo(4,5));//9 
 
-// Implicit return in arrow function
+// // Implicit return in arrow function
 
-const addthree = (num1,num2,num3) => num1+num2+num3 //This is implicit return 
-console.log(addthree(1,2,3))
+// const addthree = (num1,num2,num3) => num1+num2+num3 //This is implicit return 
+// console.log(addthree(1,2,3))//6 
 
-const addfour = (num1,num2,num3,num4) => (num1+num2+num3+num4)//This is also implicit return
-console.log(addfour(1,2,3,4));
+// const addfour = (num1,num2,num3,num4) => (num1+num2+num3+num4)//This is also implicit return
+// console.log(addfour(1,2,3,4));//10
 
-const add = (num1,num2) => ({username:"Tanish"})
-console.log(add(1,2));//{ username: 'Tanish' } This will be the output here 
+// const add = (num1,num2) => ({username:"Tanish"})//object will be returned by using ({username:"tanish"}) like this 
+// console.log(add(1,2));//{ username: 'Tanish' } This will be the output here 
 
-// const myarr = [1,2,3,4,5]
-// myarr.forEach(function(){})
-// myarr.forEach(()=>{})
-// myarr.forEach(()=>())
+// // const myarr = [1,2,3,4,5]
+// // myarr.forEach(function(){})
+// // myarr.forEach(()=>{})
+// // myarr.forEach(()=>())
 
 
 
