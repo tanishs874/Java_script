449e7f43992939840e95fa228a0c9001c246eb71
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2024-04-20 05:18:58 +0000

    done

--- .devcontainer/02_basics/03_objects.js
+++ .devcontainer/02_basics/03_objects.js
@@ -5,9 +5,9 @@
 const mysymbol = Symbol("Key 1")//Symbol 
 const jsUser = {
     name : "Tanish",
-    "full name" : "Tanish sharma",//We cannot acces this value with using dot.Sp,we need to use square notation console.log(jsUser["full name"])
-    mysymbol : "key1",//It will give the output as key1 but the type of this will be string
-    [mysymbol]: "key23",//Now it will give the output as key2 and the type will also be symbol
+    "full name" : "Tanish sharma",//We cannot acces this value with using dot.So,we need to use square notation console.log(jsUser["full name"])
+    mysymbol : "key1",//It will give the output as key1 but the typeof this will be string
+    [mysymbol]: "key23",//Now it will give the output as key2 and the typeof will also be symbol
     age:20,
     location:"Ambala",
     email:"Ts@google.com",
@@ -32,7 +32,7 @@ jsUser.greeting = function(){
 }
 // console.log(jsUser.greeting);//Undefined when object is freezed
 // console.log(jsUser.greeting)//[Function (anonymous)] when object is not freezed
-// console.log(jsUser.greeting());
+// console.log(jsUser.greeting());//Hello jsUser
 
 jsUser.greeting_two = function(){
     console.log(`Hello jsUser ,${this.name}`)
